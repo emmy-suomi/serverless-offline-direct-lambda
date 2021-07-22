fd167d724dea67dae75a005a826c8ba9b6426299
phebe-s <phebe@iona.studio>
2021-07-22 11:26:36 +0800

    Modify to only support lambda-to-lambda via aws sdk

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
 'use strict';
 
-const packagePath = 'node_modules/serverless-offline-direct-lambda';
-const handlerPath = `proxy.js`;
-
 var AWS_SDK_USED = process.env.AWS_SDK_USED || 'rails';
 function AWS_SDK_METHOD(functionBeingProxied, location) {
+  if(AWS_SDK_USED == 'node') {
 
-    if(AWS_SDK_USED == 'node') {
-
-        // Additional support to call the function from the AWS SDK (NodeJS) directly...
-        var AWS_SDK_NODE_METHOD = {
-          http: {
-            method: 'POST',
-            // This is the path to the Lambda API..
-            path: `2015-03-31/functions/${functionBeingProxied.name}/invocations`,
-            integration: 'lambda',
-            request: {
-              template: {
-                // NB: AWS SDK for NodeJS specifies as 'binary/octet-stream' not 'application/json'
-                'binary/octet-stream': JSON.stringify(
-                  {
-                    location,   
-                    body: "$input.body",
-                    targetHandler :  functionBeingProxied.handler,
-                  }
-                )
+    // Support to call the function from the AWS SDK (NodeJS) directly...
+    var AWS_SDK_NODE_METHOD = {
+      http: {
+        method: 'POST',
+        cors: true,
+        // This is the path to the Lambda API..
+        path: `/2015-03-31/functions/${functionBeingProxied.name}/invocations`,
+        request: {
+          template: {
+            // NB: AWS SDK for NodeJS specifies as 'binary/octet-stream' not 'application/json'
+            'binary/octet-stream': JSON.stringify(
+              {
+                location,   
+                body: "$input.body",
+                targetHandler: functionBeingProxied.handler,
               }
-            },
-            response: {
-              headers: {
-                "Content-Type": "application/json"
-              }
-            }
+            ),
           }
-        };
-        return AWS_SDK_NODE_METHOD;
-
-    } else {
-
-        // Additional support to call the function from the All other SDK's (Don't ask why AWS did it like this ......)
-        var AWS_SDK_RAILS_METHOD = {
-          http: {
-            method: 'POST',
-            // This is the path to the Lambda API..
-            path: `2015-03-31/functions/${functionBeingProxied.name}/invocations`,
-            integration: 'lambda',
-            request: {
-              template: {
-                // NB: AWS SDK for NodeJS specifies as 'binary/octet-stream' not 'application/json'
-                'application/json': JSON.stringify(
-                  {
-                    location,   
-                    body: "$input.json('$')",
-                    targetHandler :  functionBeingProxied.handler,
-                  }
-                )
-              }
-            },
-            response: {
-              headers: {
-                "Content-Type": "application/json"
-              }
-            }
+        },
+        response: {
+          headers: {
+            "Content-Type": "application/json"
           }
-        };
-        return AWS_SDK_RAILS_METHOD;
-    }
+        }
+      }
+    };
+    return AWS_SDK_NODE_METHOD;
+
+  } else {
+
+    // Additional support to call the function from the All other SDK's (Don't ask why AWS did it like this ......)
+    var AWS_SDK_RAILS_METHOD = {
+      http: {
+        method: 'POST',
+        // This is the path to the Lambda API..
+        path: `2015-03-31/functions/${functionBeingProxied.name}/invocations`,
+        request: {
+          template: {
+            // NB: AWS SDK for NodeJS specifies as 'binary/octet-stream' not 'application/json'
+            'application/json': JSON.stringify(
+              {
+                location,   
+                body: "$input.json('$')",
+                targetHandler :  functionBeingProxied.handler,
+              }
+            )
+          }
+        },
+        response: {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      }
+    };
+    return AWS_SDK_RAILS_METHOD;
+  }
 
 };
 
@@ -74,8 +69,11 @@ class ServerlessPlugin {
     this.serverless = serverless;
     this.options = options;
 
+    const boundStartHandler = this.startHandler.bind(this);
+
     this.hooks = {
-      "before:offline:start:init": this.startHandler.bind(this),
+      'before:offline:start': boundStartHandler,
+      'before:offline:start:init': boundStartHandler,
     };
   }
 
@@ -83,8 +81,9 @@ class ServerlessPlugin {
     let location = '';
     try {
       location = this.serverless.service.custom['serverless-offline'].location;
-      this.serverless.service.custom['serverless-offline'].location = '';
     } catch (_) { }
+
+    location = `${this.serverless.config.servicePath}/${location}`;
 
     this.serverless.cli.log('Running Serverless Offline with direct lambda support');
 
@@ -98,48 +97,26 @@ const addProxies = (functionsObject, location) => {
     // filter out functions with event config,
     // leaving just those intended for direct lambda-to-lambda invocation
     const functionObject = functionsObject[fn];
-    if (!functionObject.events || functionObject.events.length === 0) {
+    if (!functionObject.events || !functionObject.events.some((event) => Object.keys(event)[0] === 'http')) {
       const pf = functionProxy(functionObject, location);
       functionsObject[pf.name] = pf;
+    } else {
+      functionsObject[fn] = {
+        ...functionObject,
+        handler: `${location}/${functionObject.handler}`,
+      }
     }
   });
 };
 
 const functionProxy = (functionBeingProxied, location) => ({
+  ...functionBeingProxied,
   name: `${functionBeingProxied.name}_proxy`,
-  handler: `${packagePath}/proxy.handler`,
   environment: functionBeingProxied.environment,
-  events: [
-    // This is the original `/post/FUNCTION-NAME` from the plugin...
-    {
-      http: {
-        method: 'POST',
-        path: `proxy/${functionBeingProxied.name}`,
-        integration: 'lambda',
-        request: {
-          template: {
-            'application/json': JSON.stringify(
-              {
-                location,
-                body: "$input.json('$')",
-                targetHandler :  functionBeingProxied.handler,
-              }
-            )
-          }
-        },
-        response: {
-          headers: {}
-        }
-      }
-    },
- 
+  events: [ 
     // See methods above for further details
     AWS_SDK_METHOD(functionBeingProxied, location)
-
   ],
-  package: {
-    include: [handlerPath],
-  }
 });
 
 module.exports = ServerlessPlugin;
